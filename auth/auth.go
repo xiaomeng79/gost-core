c2e5288a015c19669d8a28ea06b521830220f1d5
孟庆芳 <mengqingfang@safeis.cn>
2023-05-31 17:58:03 +0800

    feat: 客户端认证

--- auth/auth.go
+++ auth/auth.go
@@ -2,9 +2,21 @@ package auth
 
 import "context"
 
+const (
+	// 不需要认证
+	AUTH_NOT_NEED int64 = 0
+	// 认证不通过
+	AUTH_NOT_PASSED int64 = -1
+)
+
+const (
+	USER_ID = "user_id"
+)
+
 // Authenticator is an interface for user authentication.
 type Authenticator interface {
-	Authenticate(ctx context.Context, user, password string) bool
+	// 0：不需要认证 -1：认证不通过
+	Authenticate(ctx context.Context, user, password string) int64
 }
 
 type authenticatorGroup struct {
@@ -17,14 +29,16 @@ func AuthenticatorGroup(authers ...Authenticator) Authenticator {
 	}
 }
 
-func (p *authenticatorGroup) Authenticate(ctx context.Context, user, password string) bool {
+func (p *authenticatorGroup) Authenticate(ctx context.Context, user, password string) int64 {
 	if len(p.authers) == 0 {
-		return true
+		return AUTH_NOT_NEED
 	}
 	for _, auther := range p.authers {
-		if auther != nil && auther.Authenticate(ctx, user, password) {
-			return true
+		if auther != nil {
+			if id := auther.Authenticate(ctx, user, password); id != AUTH_NOT_PASSED {
+				return id
+			}
 		}
 	}
-	return false
+	return AUTH_NOT_PASSED
 }
